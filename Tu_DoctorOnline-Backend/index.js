6fdd636e609fc26cb21b619ec23c9cbf499f818a
RogerHack92 <108960587+RogerHack92@users.noreply.github.com>
2022-10-23 20:49:46 -0500

    INICIO

---
+++ Tu_DoctorOnline-Backend/index.js
@@ -0,0 +1,23 @@
+//Importación
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require ('./conexion');
+
+
+//Configuración
+const app = express();
+const env = process.env;
+const port = env.PORT || 3000
+;
+//app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+//Arranque
+app.listen(port, () => {
+    console.log("API iniciado en puerto " + port);
+});
+//Rutas base
+app.get('/', (req, res) => {
+    res.send("TU DOCTOR ONLINE - BIENVENIDOS");
+});
